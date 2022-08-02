855bb14da4dba29e675e5084b8da492127e5b4fd
Alban001 <eliasalban03@gmail.com>
2022-08-02 08:10:04 -0400

    freecodecamp test added

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -20,20 +20,22 @@ const Quote = () => {
 
   return (
     
-    <div className='quote'>
-          <div className="quote__container-content">
-            {click ? <h2> <i className="fa-solid fa-quote-left"></i>{phrase}</h2> : <h2> <i className="fa-solid fa-quote-left"></i>{phrase}</h2>}
+    <div className='quote' id="quote-box">
+          <div className="quote__container-content" id="text" >
+          <div id="author">
+          {click ? <h2> <i className="fa-solid fa-quote-left"></i>{phrase}</h2> : <h2> <i className="fa-solid fa-quote-left"></i>{phrase}</h2>}
             {click ? <h3>{author}</h3> : <h3>{author}</h3>}
+          </div>
             <div className="quote__button-container">
               <div className="quote__button-left">
                   <div className="quote__button">
-                        <a href="twitter.com/intent/tweet" target='_top'><i className="fa-brands fa-square-twitter"></i></a>
+                        <a id="tweet-quote" href="twitter.com/intent/tweet" target='_top'><i className="fa-brands fa-square-twitter"></i></a>
                   </div>
                   <div className="quote__button">
                         <a href="https://www.tumblr.com/" target='_blank'><i className="fa-brands fa-tumblr"></i></a>
                   </div> 
                 </div>
-               <div className="quote__button-right">
+               <div className="quote__button-right" id="new-quote">
                <button onClick={handleChange}>Nueva Frase</button>
                </div>
             </div>
